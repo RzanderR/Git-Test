c33e2aafae09f0c47d9b97deb8c06a3115cf2ffc
VRV2130 <63615458+VRV2130@users.noreply.github.com>
2023-05-15 19:19:21 +0000

    added Math

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-i  = "JOE ROGAN"
-z = "3"
-print("Vikash: I LOVE {x, z}")
-print("OE")
+import math
+def quadformula(a,b,c):
+  try:
+    s = math.sqrt((b^2)-(4*a*c))
+  except:
+    return "no"
+  x1 = (-b+s)/(2*a)
+  x2 = (-b-s)/(2*a)
+  return x1,x2
+
+quadformula(1,2,4)
