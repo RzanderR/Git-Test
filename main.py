ec9775f387c1117703157c18c2b07490e8609c9d
RzanderR <zander.rothstein@gmail.com>
2023-05-15 21:59:30 +0000

    Yippee! Quadratic Formula! Yippee! Hooray!

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import math
+
 def quadformula(a,b,c):
   try:
-    s = math.sqrt((b^2)-(4*a*c))
+    s = math.sqrt((b**2)-(4*a*c))
   except:
-    return "no"
+    print('no')
+  s = math.sqrt((b**2)-(4*a*c))
   x1 = (-b+s)/(2*a)
   x2 = (-b-s)/(2*a)
   print(x1)
   print(x2)
 
-quadformula(1,2,4)
+quadformula(1,-4,4)
